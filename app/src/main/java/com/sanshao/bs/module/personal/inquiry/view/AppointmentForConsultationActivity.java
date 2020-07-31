2e77d251757575cbc54df78642826a8f21afec09
yuexingxing <670176656@qq.com>
2020-07-31 16:31:40 +0800

    ui

--- app/src/main/java/com/sanshao/bs/module/personal/inquiry/view/AppointmentForConsultationActivity.java
+++ app/src/main/java/com/sanshao/bs/module/personal/inquiry/view/AppointmentForConsultationActivity.java
@@ -1,28 +1,20 @@
 package com.sanshao.bs.module.personal.inquiry.view;
 
-import android.content.ClipData;
-import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
 import android.view.View;
 
 import com.exam.commonbiz.base.BaseActivity;
-import com.exam.commonbiz.util.CommonCallBack;
 import com.exam.commonbiz.util.ContainerUtil;
 import com.sanshao.bs.R;
-import com.sanshao.bs.SSApplication;
 import com.sanshao.bs.databinding.ActivityAppointmentForConsultationBinding;
 import com.sanshao.bs.module.order.bean.ConfirmOrderResponse;
 import com.sanshao.bs.module.order.bean.StoreInfo;
 import com.sanshao.bs.module.order.model.IConfirmOrderModel;
 import com.sanshao.bs.module.order.view.adapter.ConfirmOrderAdapter;
-import com.sanshao.bs.module.order.view.dialog.SelectSubscribeTimeDialog;
-import com.sanshao.bs.module.order.viewmodel.AppointmentForConsultationViewModel;
 import com.sanshao.bs.module.order.viewmodel.ConfirmOrderViewModel;
-import com.sanshao.bs.module.personal.bean.UserInfo;
 import com.sanshao.bs.module.personal.personaldata.dialog.SelectBirthdayDialog;
-import com.sanshao.bs.util.OpenLocalMapUtil;
-import com.sanshao.bs.util.ToastUtil;
+import com.sanshao.bs.util.CommandTools;
 import com.sanshao.commonui.titlebar.OnTitleBarListener;
 
 /**
@@ -64,25 +56,16 @@ public class AppointmentForConsultationActivity extends BaseActivity<ConfirmOrde
 
             }
         });
-        binding.tvCopy.setOnClickListener(v -> {
-            //获取剪贴板管理器：
-            ClipboardManager cm = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-            ClipData mClipData = ClipData.newPlainText("Label", binding.tvOrderNo.getText().toString());
-            cm.setPrimaryClip(mClipData);
-            ToastUtil.showShortToast("复制成功");
-        });
+        binding.tvCopy.setOnClickListener(v -> CommandTools.copyToClipboard(context, binding.tvOrderNo.getText().toString()));
         binding.rlSubscribeTime.setOnClickListener(v -> {
             boolean[] timeType = new boolean[]{true, true, true, true, true, false};
             new SelectBirthdayDialog()
                     .init(context, "预约时间", timeType)
-                    .setCommonCallBack(new CommonCallBack() {
-                        @Override
-                        public void callback(int postion, Object object) {
-                            if (object == null) {
-                                return;
-                            }
-                            binding.tvSubscribeTime.setText((String) object);
+                    .setCommonCallBack((postion, object) -> {
+                        if (object == null) {
+                            return;
                         }
+                        binding.tvSubscribeTime.setText((String) object);
                     }).show();
         });
         binding.btnSubscribe.setOnClickListener(v -> {
