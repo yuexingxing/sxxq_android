281a2c934752d7efb89267054da6ae94b58cdb9a
yuexingxing <670176656@qq.com>
2020-08-11 16:33:30 +0800

    配置wxapi

--- app/src/main/java/com/sanshao/bs/util/Constants.java
+++ app/src/main/java/com/sanshao/bs/util/Constants.java
@@ -26,6 +26,7 @@ public class Constants {
     public static final int PAGE_SIZE = 10;
     public static final String OPT_TYPE = "opt_type";
     public static final String OPT_DATA = "opt_data";
+    public static final String WX_APPId = "";
 
     public static final String DEFAULT_IMG_BG = "http://jzvd-pic.nathen.cn/jzvd-pic/00b026e7-b830-4994-bc87-38f4033806a6.jpg";
     public static final String DEFAULT_IMG_URL = "http://img.cyw.com/shopx/20130606155913125664/shopinfo/201605041441522.jpg";
