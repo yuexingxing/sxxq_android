dab12cb8e35bee6e8232e539a06ac177b9e5fe95
yuexingxing <670176656@qq.com>
2020-09-26 03:52:57 +0800

    buidtools sdk改为28

--- app/src/main/java/cn/sanshaoxingqiu/ssbm/module/home/view/adapter/LiveTabFragmentAdapter.java
+++ app/src/main/java/cn/sanshaoxingqiu/ssbm/module/home/view/adapter/LiveTabFragmentAdapter.java
@@ -6,7 +6,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
